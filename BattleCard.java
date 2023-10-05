4c300a494f353e75fc998c7eb2bc8c0a480625be
Beni <benilevi2005@gmail.com>
2023-10-05 14:11:23 +0200

    Created instance variables for the BattleCard class.

--- BattleCard.java
+++ BattleCard.java
@@ -1,3 +1,40 @@
 public class BattleCard implements CardInterface {
-    
+    public String name;
+    public int hitponits;
+    public int attack;
+    public int defence;
+    public int manaCost;
+
+    public BattleCard(){
+        this.name = name;
+        this.hitponits = hitponits;
+        this.attack = attack;
+        this.defence = defence;
+        this.manaCost = manaCost;
+    }
+
+    @Override
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void setHitpoints(int hitponits){
+        this.hitponits = hitponits;
+    }
+
+    @Override
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    @Override
+    public void setDefence(int defence){
+        this.defence = defence;
+    }
+
+    @Override
+    public void setManaCost(int manaCost){
+        this.manaCost = manaCost;
+    }
 }
--- CardInterface.java
+++ CardInterface.java
@@ -1,3 +1,7 @@
 public interface CardInterface {
-    
+    void setName(String name);
+    void setHitpoints(int hitponits);
+    void setAttack(int attack);
+    void setDefence(int defence);
+    void setManaCost(int manaCost);
 }
